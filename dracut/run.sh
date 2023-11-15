15d9c932ecebc1b8c2b01eb7e1110e9d4dbb4cd2
Pavel Valena <pvalena@redhat.com>
2023-11-15 16:26:57 +0100

    run.sh: add docs
    and ensure there's no http_proxy

--- dracut/run.sh
+++ dracut/run.sh
@@ -1,7 +1,10 @@
 #!/usr/bin/zsh
+# ./run.sh BREW_BUILD REF_ISSUE
 
 set -e
 zsh -n "$0"
+
+export http_proxy=
 
 [[ '-d' == "$1" ]] && {
   shift
@@ -20,13 +23,15 @@ cd "$b"
 
 [[ "$b" == "$(basename "$PWD")" ]]
 
+pwd
+
 rm *.rpm||:
 
 brew download-task "$n"
 
 git add *.rpm
 
-git commit -am "Add rpms for #${b}."
+git commit -am "Add rpms for RHEL-${b}."
 
 git push
 
